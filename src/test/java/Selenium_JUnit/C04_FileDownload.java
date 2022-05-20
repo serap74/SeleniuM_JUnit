963a2353bee9ed24b84b6b8384445325d6eaccf8
Serap Diraz <emeksevde74@gmail.com>
2022-05-21 00:22:33 +0300

    21.05.22

---
+++ src/test/java/Selenium_JUnit/C04_FileDownload.java
@@ -0,0 +1,35 @@
+package Selenium_JUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utulities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C04_FileDownload extends TestBase {
+    @Test
+    public void Test01() throws InterruptedException {
+
+        //2. https://the-internet.herokuapp.com/download adresine gidelim.
+        driver.get("https://the-internet.herokuapp.com/download");
+        //3. dummy.txt dosyasını indirelim
+        WebElement dummy=driver.findElement(By.xpath("//*[text()='dummy.txt']"));
+        dummy.click();
+        Thread.sleep(5000);
+        //4. dosyanın başarıyla indirilip indirilmediğini test edelim
+
+        String farkliKisim=System.getProperty("user.home");
+        String ortakKisim="\\Downloads\\dummy.txt";
+
+        String arananDosyaYolu=farkliKisim+ortakKisim;
+
+        Assert.assertTrue(Files.exists(Paths.get(arananDosyaYolu)));
+
+
+
+
+    }
+}
