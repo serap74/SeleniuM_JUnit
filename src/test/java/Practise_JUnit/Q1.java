3749a495a61ed6b10991bcb9f2292115b9209a86
Serap Diraz <emeksevde74@gmail.com>
2022-05-20 09:58:29 +0300

    20.05.22

---
+++ src/test/java/Practise_JUnit/Q1.java
@@ -0,0 +1,69 @@
+package Practise_JUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Q1 {
+
+        /*
+    ...Exercise1...
+    BeforeClass ile driver ı olusturun ve class icinde static yapin
+    Maximize edin, 15 sn bekletin
+    http://www.google.com adresine gidin
+    arama kutusuna "Green Mile" yazip, cikan sonuc sayisini yazdirin
+    arama kutusuna  "Premonition" yazip, cikan sonuc sayisini yazdirin
+    arama kutusuna  "The Curious Case of Benjamin Button" yazip, cikan sonuc sayisini yazdirin
+    AfterClass ile kapatın
+
+ */
+        static WebDriver driver;
+        @BeforeClass
+        public static void setUp() {
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+            driver.get("http://www.google.com");
+
+        }
+                @Before
+            public void testtenance(){
+                    driver.get("http://www.google.com");
+                }
+                @Test
+            public void test01(){
+            WebElement aramaKutusu=driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+            aramaKutusu.sendKeys("Green Mile");
+            aramaKutusu.submit();
+        }
+        @Test
+        public void test02() {
+            WebElement aramaKutusu=driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+            aramaKutusu.sendKeys("Premonition" + Keys.ENTER);
+        }
+        @Test
+        public void test03() {
+            WebElement aramaKutusu=driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+            aramaKutusu.sendKeys("The Curious Case of Benjamin Button" + Keys.ENTER);
+
+        }
+        @After
+        public void testtenSonra(){
+            WebElement sonucYazisiElementi=driver.findElement(By.xpath("//div[@id='result-stats']"));
+            System.out.println("sonucYazisiElementi = " + sonucYazisiElementi.getText());
+
+        }
+        @AfterClass
+    public static void tearDown() {
+            driver.quit();
+        }
+    }
+
